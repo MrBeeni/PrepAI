9c7bcd4d91d5b289c162292857c252a2788ad8c4
MrBeeni <mubeen4429@gmail.com>
2025-03-27 12:10:23 +0500

    implemented interview generation page ui

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,21 @@
+import { getCurrentUser } from "@/actions/auth.action";
+import Agent from "@/components/Agent";
+
+const Page = async () => {
+  const user = await getCurrentUser();
+
+  return (
+    <>
+      <h3>Interview generation</h3>
+
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        // profileImage={"user?.profileURL"}
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
